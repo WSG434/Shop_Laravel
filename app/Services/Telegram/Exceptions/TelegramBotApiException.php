a60118cbf37f3d65395e7725a4aef57ffd359df5
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-05-02 15:52:28 +0300

    Exceptions. Отработка исключений и отправка сообщений в Telegram

---
+++ app/Services/Telegram/Exceptions/TelegramBotApiException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Telegram\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+
+final class TelegramBotApiException extends Exception
+{
+    public function render(Request $request)
+    {
+        return response()->json([]);
+    }
+}
--- config/logging.php
+++ config/logging.php
@@ -55,7 +55,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'telegram'],
             'ignore_exceptions' => false,
         ],
 
