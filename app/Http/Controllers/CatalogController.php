dfe76ad3b95caf2639fa049b944af84614d69f94
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-05-09 14:30:43 +0300

    Реализация ViewModels подхода для Каталога

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,33 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\View\ViewModels\CatalogViewModel;
 use Domain\Catalog\Models\Category;
 use Domain\Product\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 
 class CatalogController extends Controller
 {
-
-
    public function __invoke(?Category $category)
    {
-       $categories = Category::query()
-           ->select(['id', 'title', 'slug'])
-           ->has('products')
-           ->get();
-
-       $products = Product::query()
-           ->select(['id', 'title', 'slug', 'price', 'thumbnail', 'json_properties'])
-           ->search()
-           ->withCategory($category)
-           ->filtered()
-           ->sorted()
-           ->paginate(6);
-
-       return view('catalog.index', [
-           'products' => $products,
-           'categories' => $categories,
-           'category' => $category
-       ]);
+       return (new CatalogViewModel($category))
+           ->view('catalog.index');
    }
 }
